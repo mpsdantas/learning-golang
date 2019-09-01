1eb3827a22c1031110e0f0b281feaa0361cebddf
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2019-09-01 19:39:41 -0300

    Casting and const

---
+++ casting.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 46
+	//b := 6.4
+	var c float64 = float64(a)
+	fmt.Println(c)
+}
---
+++ const.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const Pi = 3.14
+	var number = math.Max(3, Pi)
+	fmt.Println(number)
+}
