074e18a1d16c3477d0df2504d65f6752a4c01a1d
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2019-09-01 19:13:18 -0300

    First commit

---
+++ basicTypes.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var test1 int = 1
+	// var test2 int8 = 1
+	// var test3 int16 = 1
+	// var test4 int32 = 1
+	// var test5 uint = 1
+	// var test6 uint8 = 1
+	// var test7 uint16 = 1
+	// var test8 uint32 = 1
+	// var test9 uint64 = 1
+	// var test10 string = "Oi"
+	// var test11 float32 = 1.1
+	// var test12 float64 = 1.2
+	// var test13 bool = false
+	// var test14 complex64
+	// var test15 complex128
+	// var test16 byte
+	// var test17 rune
+
+	fmt.Printf("Type of test1: %T, value: %v", test1, test1)
+}
---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var (
+	// 	age  = 22
+	// 	name = "Marcus Dantas"
+	// )
+	age, name := 35, "Marcus Dantas"
+	fmt.Println("My name is", name, "and my age is:", age)
+}
